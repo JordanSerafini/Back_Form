2856c3e5882a50c284fe6bcaae4b83e17d424958
JordanSerafini <jordan@solution-logique.fr>
2024-02-07 17:27:05 +0100

    :sparkles: all ok

--- src/database/dbPG.sql
+++ src/database/dbPG.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+-- Création de la table "user"
 CREATE TABLE IF NOT EXISTS "user" (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
@@ -7,31 +8,35 @@ CREATE TABLE IF NOT EXISTS "user" (
   date DATE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS question (
-  id SERIAL PRIMARY KEY,
-  formID INTEGER NOT NULL,
-  questionID INTEGER NOT NULL,
-  rating INTEGER NOT NULL,
-  userID INTEGER NOT NULL REFERENCES "user"(id) --
-);
-
-CREATE TABLE IF NOT EXISTS comment (
-  id SERIAL PRIMARY KEY,
-  formID INTEGER NOT NULL,
-  comment TEXT NOT NULL,
-  userID INTEGER NOT NULL REFERENCES "user"(id) --
-);
-
+-- Création de la table "utilisateur"
 CREATE TABLE IF NOT EXISTS utilisateur (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  email TEXT NOT NULL,
-  password TEXT NOT NULL,
-);
+  email TEXT UNIQUE NOT NULL, -- Assure l'unicité des emails
+  password TEXT NOT NULL
+); 
 
+-- Création de la table "form"
 CREATE TABLE IF NOT EXISTS form (
   id SERIAL PRIMARY KEY,
-  userID INTEGER NOT NULL REFERENCES "user"(id) 
-  
+  userID INTEGER NOT NULL REFERENCES "user"(id)
 );
+
+-- Correction de la table "question"
+CREATE TABLE IF NOT EXISTS question (
+  id SERIAL PRIMARY KEY,
+  formID INTEGER NOT NULL REFERENCES "form"(id), --
+  questionID INTEGER NOT NULL, 
+  rating INTEGER NOT NULL,
+  userID INTEGER NOT NULL REFERENCES "user"(id) -- Suppression du commentaire "--"
+);
+
+-- Correction de la table "comment"
+CREATE TABLE IF NOT EXISTS comment (
+  id SERIAL PRIMARY KEY,
+  formID INTEGER NOT NULL REFERENCES "form"(id),
+  comment TEXT NOT NULL,
+  userID INTEGER NOT NULL REFERENCES "user"(id)
+);
+
 COMMIT;
