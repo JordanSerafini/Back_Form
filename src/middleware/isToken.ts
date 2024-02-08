fa5119436428b89071a8d576039dfd97a502d970
JordanSerafini <jordan@solution-logique.fr>
2024-02-08 17:22:40 +0100

    :construction:

--- src/middleware/isToken.ts
+++ src/middleware/isToken.ts
@@ -3,16 +3,16 @@ import jwt from 'jsonwebtoken';
 
 const isToken = (req: any, res:any, next:any) => {
   
-  const token = req.query.token; // Extraire le token de l'URL
-  if (!token) return res.sendStatus(401); // Token manquant
+  const token = req.query.token; 
+  if (!token) return res.sendStatus(401); 
 
-  const jwtSecret = process.env.SECRET_KEY; // Get the JWT secret from environment variable
-  if (!jwtSecret) return res.sendStatus(500); // JWT secret is not defined
+  const jwtSecret = process.env.SECRET_KEY; 
+  if (!jwtSecret) return res.sendStatus(500); 
 
   jwt.verify(token, jwtSecret, (err: any, user:any) => {
-    if (err) return res.sendStatus(403); // Token invalide
+    if (err) return res.sendStatus(403);
     req.user = user;
-    next(); // Passer au middleware suivant
+    next(); 
   });
 };
 
