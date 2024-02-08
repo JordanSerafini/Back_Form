519113a436c9a24c7f64178677989398545fb6dc
JordanSerafini <jordan@solution-logique.fr>
2024-02-08 13:36:55 +0100

    :sparkles: first dialy commit

--- src/database/query.sql
+++ src/database/query.sql
@@ -3,3 +3,24 @@
 SELECT question.*, "user".name AS username
 FROM question
 JOIN "user" ON question.userid = "user".id;
+
+--Récupérer les informations des utilisateurs et leurs formulaires
+SELECT u.id AS user_id, u.name AS user_name, u.fonction, u.date, f.id AS form_id
+FROM "user" u
+JOIN form f ON u.id = f.userID;
+
+--Récupérer tous les formulaires avec leurs questions et notes
+SELECT f.id AS form_id, q.questionID, q.rating
+FROM form f
+JOIN question q ON f.id = q.formID;
+
+--Récupérer les commentaires pour chaque formulaire
+SELECT f.id AS form_id, c.comment, u.name AS user_name, u.fonction
+FROM comment c
+JOIN form f ON c.formID = f.id
+JOIN "user" u ON c.userID = u.id;
+
+--Récupérer les informations d'utilisateurs avec leurs questions et réponses
+SELECT u.id AS user_id, u.name AS user_name, u.fonction, q.questionID, q.rating
+FROM "user" u
+JOIN question q ON u.id = q.userID;
