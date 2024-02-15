75195423e5857f56a893b6274f2e5ff758bf8768
JordanSerafini <jordan@solution-logique.fr>
2024-02-15 15:31:06 +0100

    insert event in sql

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -14,11 +14,11 @@ const eventController = {
         res.status(500).send("Erreur lors de la récupération des données.");
       }
   },
-  
+
   async insertEvent(req: any, res: any) {
     try {
       const { caption, startdatetime, enddatetime, expectedduration_durationinhours, achievedduration_durationinhours, colleagueId, notesclear, xx_type_tache } = req.body;
-      const query = `INSERT INTO event (Caption, StartDateTime, EndDateTime, ExpectedDuration_DurationInHours, AchievedDuration_DurationInHours, ColleagueId, NotesClear, xx_Type_Tache)
+      const query = `INSERT INTO event (caption, startdatetime, enddatetime, expectedduration_durationinhours, achievedduration_durationinhours, colleagueId, notesclear, xx_type_tache)
       VALUES ('${caption}', '${startdatetime}', '${enddatetime}', ${expectedduration_durationinhours}, ${achievedduration_durationinhours}, '${colleagueId}', '${notesclear}', '${xx_type_tache}')`;
       await pool.query(query);
       res.send("Evenement ajouté avec succès");
